46e62ea2fdc6dc686d23b6fe312dfa89007a1bcb
rtspw <richard.cx.tang@gmail.com>
2020-03-02 17:21:37 -0800

    Generalized offset square shape and added noise option for randomness

--- shapes/offset-square.js
+++ shapes/offset-square.js
@@ -1,25 +1,62 @@
 import { tiny } from '../tiny-graphics.js';
 const { Shape, vec3 } = tiny;
 
+/**
+ * options: Object
+ * |-columnDivisions: number - Greater than 0
+ * |-rowDivisions: number - Greater than 0
+ * |-bumpiness: number - Between 0 and 1
+ * |   Vertex offsets normal to flat square shape
+ * |-colNoiseFactor: number - Between 0 and 1
+ * |   Randomness in location of column vertices
+ * |-rowNoiseFactor: number - Between 0 and 1
+ * |   Randomness in location of row vertices
+ */
 class OffsetSquare extends Shape {
-  constructor(divisions = 6, bumpiness = 1) { 
+  constructor(options = {}) {
+    const {
+      columnDivisions = 6,
+      rowDivisions = 6,
+      bumpiness = 1,
+      colNoiseFactor = 0.5,
+      rowNoiseFactor = 0.5,
+    } = options;
     super( "position", "normal");
-    const dividedSquare = [...Array(divisions)].map((x, i) => ((2/divisions) * i) - 1);
-    dividedSquare.push(1);
-    const topVerticies = dividedSquare.map(x => [x,1,Math.random() * bumpiness]);
-    const bottomVerticies = dividedSquare.map(x => [x,-1,Math.random() * bumpiness]);
+    const columnDivisionIndices = this.subdivideNegOneToOne(columnDivisions)
+    const rowDivisionIndicies = this.subdivideNegOneToOne(rowDivisions);
+    const vertexRows = rowDivisionIndicies
+      .reverse()
+      .map((rowIndex) => {
+        return columnDivisionIndices.map(colIndex => {
+          const colNoise = colIndex === 1 || colIndex === -1 ? 0 : this.generateNoise(columnDivisions, colNoiseFactor);
+          const rowNoise = rowIndex === 1 || rowIndex === -1 ? 0 : this.generateNoise(rowDivisions, rowNoiseFactor);
+          return [colIndex + colNoise, rowIndex + rowNoise, Math.random() * bumpiness];
+        })
+      });
     const verticies = [];
     const normals = [];
-    for (let i = 0; i < topVerticies.length - 1; i++) {
-      verticies.push(vec3(...topVerticies[i + 1]), vec3(...topVerticies[i]), vec3(...bottomVerticies[i]));
-      const firstNormal = this.getNormalFromTopOfVertexList(verticies);
-      normals.push(firstNormal, firstNormal, firstNormal);
-      verticies.push(vec3(...bottomVerticies[i]), vec3(...bottomVerticies[i + 1]), vec3(...topVerticies[i + 1]));
-      const secondNormal = this.getNormalFromTopOfVertexList(verticies);
-      normals.push(secondNormal, secondNormal, secondNormal);
+    for (let i = 0; i < columnDivisions; i++) {
+      for (let j = 0; j < rowDivisions; j++) {
+        verticies.push(vec3(...vertexRows[j][i+1]), vec3(...vertexRows[j][i]), vec3(...vertexRows[j+1][i]));
+        const firstNormal = this.getNormalFromTopOfVertexList(verticies);
+        normals.push(firstNormal, firstNormal, firstNormal);
+        verticies.push(vec3(...vertexRows[j+1][i]), vec3(...vertexRows[j+1][i+1]), vec3(...vertexRows[j][i+1]));
+        const secondNormal = this.getNormalFromTopOfVertexList(verticies);
+        normals.push(secondNormal, secondNormal, secondNormal);
+      }
     }
     this.arrays.position = verticies;
     this.arrays.normal = normals;
+  }
+
+  generateNoise(numOfDivisions, noiseFactor) {
+    return (((1 / numOfDivisions) * 2 * Math.random() - (1 / numOfDivisions))) * noiseFactor;
+  }
+
+  subdivideNegOneToOne(numOfDivisions) {
+    const divisions = [...Array(numOfDivisions)].map((_, i) => ((2/numOfDivisions) * i) - 1);
+    divisions.push(1);
+    return divisions;
   }
 
   getNormalFromTopOfVertexList(verticies) {
